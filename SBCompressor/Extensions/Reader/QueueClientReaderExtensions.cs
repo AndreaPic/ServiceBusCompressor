368da076b8940f9ab29f3449b38e4e59dc278c1b
Andrea Piccioni <andrea.piccioni.ap@gmail.com>
2022-02-26 22:31:16 +0100

    wip

--- SBCompressor/Extensions/Reader/QueueClientReaderExtensions.cs
+++ SBCompressor/Extensions/Reader/QueueClientReaderExtensions.cs
@@ -18,7 +18,7 @@ namespace SBCompressor.Extensions.Reader
     /// Extensions for use this library functionality directly form Microsoft.Azure.ServiceBus.QueueClient
     /// </summary>
     public static class QueueClientReaderExtensions
-    {          
+    {
         /// <summary>
         /// Subscribe action to read queue messages
         /// </summary>
@@ -27,24 +27,44 @@ namespace SBCompressor.Extensions.Reader
         /// <param name="serviceBusConnectionStringName">Queue connection string name (must be present in sbcsettings.json file)</param>
         /// <param name="onMessageReceived">Action invoked when message arrive</param>
         /// <returns></returns>
+#if NETCOREAPP3_1 || NET5_0
         public static void SubscribeCompressor(this IQueueClient queueClient, 
             Action<MessageReceivedEventArgs> onMessageReceived)
         {
             ReaderExtender<IQueueClient> reader = new ReaderExtender<IQueueClient>(queueClient);
             reader.Subscribe(onMessageReceived);
         }
+#endif
+#if NET6_0
+        public static void SubscribeCompressor(this ServiceBusReceiver queueClient,
+            Action<MessageReceivedEventArgs> onMessageReceived)
+        {
+            ReaderExtender<ServiceBusReceiver> reader = new ReaderExtender<ServiceBusReceiver>(queueClient);
+            reader.Subscribe(onMessageReceived);
+        }
+#endif
         /// <summary>
         /// Subscribe action to read queue messages
         /// </summary>
         /// <param name="queueClient">Type to extend</param>
         /// <param name="onMessageReceived">Action invoked when message arrive</param>
         /// <param name="settingData">Setting infomrations</param>
+#if NETCOREAPP3_1 || NET5_0
         public static void SubscribeCompressor(this IQueueClient queueClient,
             Action<MessageReceivedEventArgs> onMessageReceived, StorageSettingData settingData)
         {
             ReaderExtender<IQueueClient> reader = new ReaderExtender<IQueueClient>(queueClient, settingData);
             reader.Subscribe(onMessageReceived);
         }
+#endif
+#if NET6_0
+        public static void SubscribeCompressor(this ServiceBusReceiver queueClient,
+            Action<MessageReceivedEventArgs> onMessageReceived, StorageSettingData settingData)
+        {
+            ReaderExtender<ServiceBusReceiver> reader = new ReaderExtender<ServiceBusReceiver>(queueClient, settingData);
+            reader.Subscribe(onMessageReceived);
+        }
+#endif
 
     }
 }
--- SBCompressor/Extensions/Sender/TopicClientSenderExtensions.cs
+++ SBCompressor/Extensions/Sender/TopicClientSenderExtensions.cs
@@ -26,18 +26,38 @@ namespace SBCompressor.Extensions.Sender
         /// <param name="serviceBusConnectionStringName">Topic connection string name (must be present in sbcsettings.json file)</param>
         /// <param name="message">Message for the queue (can be json)</param>
         /// <returns></returns>
+#if NETCOREAPP3_1 || NET5_0
         public static async Task SendCompressorAsync(this ITopicClient topicClient,
             string message)
         {
             SenderExtender<ITopicClient> topicConnector = new SenderExtender<ITopicClient>(topicClient);
             await topicConnector.SendAsync(message);
         }
+#endif
+#if NET6_0
+        public static async Task SendCompressorAsync(this ServiceBusSender topicClient,
+            string message)
+        {
+            SenderExtender<ServiceBusSender> topicConnector = new SenderExtender<ServiceBusSender>(topicClient);
+            await topicConnector.SendAsync(message);
+        }
+#endif
+#if NETCOREAPP3_1 || NET5_0
         public static async Task SendCompressorAsync(this ITopicClient topicClient,
             string message, StorageSettingData settingData)
         {
             SenderExtender<ITopicClient> topicConnector = new SenderExtender<ITopicClient>(topicClient, settingData);
             await topicConnector.SendAsync(message);
         }
+#endif
+#if NET6_0
+        public static async Task SendCompressorAsync(this ServiceBusSender topicClient,
+            string message, StorageSettingData settingData)
+        {
+            SenderExtender<ServiceBusSender> topicConnector = new SenderExtender<ServiceBusSender>(topicClient, settingData);
+            await topicConnector.SendAsync(message);
+        }
+#endif
 
         /// <summary>
         /// Send message to the Topic
@@ -47,18 +67,38 @@ namespace SBCompressor.Extensions.Sender
         /// <param name="message"></param>
         /// <param name="message">object to send to service bus.</param>
         /// <returns></returns>
+#if NETCOREAPP3_1 || NET5_0
         public static async Task SendCompressorAsync<TMessage>(this ITopicClient queueClient, TMessage message)
             where TMessage : class, new()
         {
             SenderExtender<ITopicClient> queueConnector = new SenderExtender<ITopicClient>(queueClient);
             await queueConnector.SendAsync(message);
         }
+#endif
+#if NET6_0
+        public static async Task SendCompressorAsync<TMessage>(this ServiceBusSender queueClient, TMessage message)
+            where TMessage : class, new()
+        {
+            SenderExtender<ServiceBusSender> queueConnector = new SenderExtender<ServiceBusSender>(queueClient);
+            await queueConnector.SendAsync(message);
+        }
+#endif
+#if NETCOREAPP3_1 || NET5_0
         public static async Task SendCompressorAsync<TMessage>(this ITopicClient queueClient, TMessage message, StorageSettingData settingData)
             where TMessage : class, new()
         {
             SenderExtender<ITopicClient> queueConnector = new SenderExtender<ITopicClient>(queueClient, settingData);
             await queueConnector.SendAsync(message);
         }
+#endif
+#if NET6_0
+        public static async Task SendCompressorAsync<TMessage>(this ServiceBusSender queueClient, TMessage message, StorageSettingData settingData)
+            where TMessage : class, new()
+        {
+            SenderExtender<ServiceBusSender> queueConnector = new SenderExtender<ServiceBusSender>(queueClient, settingData);
+            await queueConnector.SendAsync(message);
+        }
+#endif
 
     }
 }
