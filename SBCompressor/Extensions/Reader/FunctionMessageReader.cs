0f00c58193f0896dce149adffcee0e480c420e98
Andrea Piccioni <a.piccioni@ntsinformatica.it>
2022-08-25 10:55:18 +0200

    test func reader

---
+++ SBCompressor/Extensions/Reader/FunctionMessageReader.cs
@@ -0,0 +1,182 @@
+﻿#if NET6_0
+using Azure.Messaging.ServiceBus;
+#endif
+#if NETCOREAPP3_1 || NET5_0
+using Microsoft.Azure.ServiceBus;
+#endif
+using SBCompressor.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SBCompressor.Extensions.Reader
+{
+    /// <summary>
+    /// This class can reveceive message from service bus and take care of very large message and its strategy.
+    /// </summary>
+    public sealed class FunctionMessageReader : MessageReader
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public FunctionMessageReader()
+        {
+        }
+        /// <summary>
+        /// Initialize new instance with explicit setting informations for very large message
+        /// </summary>
+        /// <param name="settingData">Setting informations for very large message</param>
+        public FunctionMessageReader(StorageSettingData settingData) : base(settingData)
+        {
+        }
+        /// <summary>
+        /// Initialize new instance with explicit setting informations for very large message
+        /// </summary>
+        /// <param name="settingData">Setting informations for very large message</param>
+        /// <param name="typeToDeserialize">Type used to deserialize message</param>        
+        public FunctionMessageReader(StorageSettingData settingData, Type typeToDeserialize) : base(settingData, typeToDeserialize)
+        {
+        }
+        /// <summary>
+        /// Initialize new instance with explicit setting informations for very large message
+        /// </summary>
+        /// <param name="typeToDeserialize">Type used to deserialize message</param>        
+        public FunctionMessageReader(Type typeToDeserialize) : base(typeToDeserialize)
+        {
+        }
+
+
+        /// <summary>
+        /// Subscribe an action for reading message from queue or topic with Azure Function
+        /// </summary>
+        /// <param name="receivedMessage">Original message triggered by Azure Function</param>
+        /// <param name="onMessageReceived">Action invoked when a message arrive</param>
+        /// <returns></returns>
+#if NET5_0 || NETCOREAPP3_1
+        public async Task SubScribe(Message receivedMessage, Action<MessageReceivedEventArgs> onMessageReceived)
+#endif
+#if NET6_0
+        public async Task SubScribe(ServiceBusReceivedMessage receivedMessage, Action<MessageReceivedEventArgs> onMessageReceived)
+#endif
+        {
+            OnMessageReceived = onMessageReceived;
+#if NET5_0 || NETCOREAPP3_1
+            CancellationToken token = new CancellationToken();
+            await MessageReceivedHandler(receivedMessage, token);
+#endif
+#if NET6_0
+            await MessageReceivedHandler(receivedMessage);
+#endif
+        }
+        /// <summary>
+        /// Subscribe an action for reading message from queue or topic with Azure Function
+        /// </summary>
+        /// <param name="receivedMessage">Original message triggered by Azure Function</param>
+        /// <param name="onMessageReceived">Action invoked when a message arrive</param>
+        /// <param name="token">CanellationToken</param>
+        /// <returns></returns>
+#if NET5_0 || NETCOREAPP3_1
+        public async Task SubScribe(Message receivedMessage, Action<MessageReceivedEventArgs> onMessageReceived, CancellationToken token)
+#endif
+#if NET6_0
+        public async Task SubScribe(ServiceBusReceivedMessage receivedMessage, Action<MessageReceivedEventArgs> onMessageReceived, CancellationToken token)
+#endif
+        {
+            //try
+            //{
+            OnMessageReceived = onMessageReceived;
+#if NET5_0 || NETCOREAPP3_1
+            await MessageReceivedHandler(receivedMessage, token);
+#endif
+#if NET6_0
+            await MessageReceivedHandler(receivedMessage);
+#endif
+            //}
+            //catch
+            //{
+
+            //}
+        }
+
+#if NET5_0 || NET6_0
+        /// <summary>
+        /// Subscribe an action for reading message from queue or topic with Azure Function
+        /// </summary>
+        /// <param name="functionData">Object hold infomration retrived by input binding of function</param>
+        /// <param name="onMessageReceived">Action invoked when a message arrive</param>
+        /// <returns></returns>
+        public async Task SubScribe(FunctionInputData functionData, Action<MessageReceivedEventArgs> onMessageReceived)
+        {
+            OnMessageReceived = onMessageReceived;
+            CancellationToken token = new CancellationToken();
+            await MessageReceivedHandler(functionData, token);
+        }
+        /// <summary>
+        /// Subscribe an action for reading message from queue or topic with Azure Function
+        /// </summary>
+        /// <param name="functionData">Object hold infomration retrived by input binding of function</param>
+        /// <param name="onMessageReceived">Action invoked when a message arrive</param>
+        /// <param name="token">CanellationToken</param>
+        /// <returns></returns>
+        public async Task SubScribe(FunctionInputData functionData, Action<MessageReceivedEventArgs> onMessageReceived, CancellationToken token)
+        {
+            //try
+            //{
+                OnMessageReceived = onMessageReceived;
+                await MessageReceivedHandler(functionData, token);
+            //}
+            //catch
+            //{
+
+            //}
+        }
+#endif
+    }
+
+#if NET5_0 || NET6_0
+
+    /// <summary>
+    /// This DTO contains data from inputbinding of the function
+    /// </summary>
+    public record FunctionInputData
+    {
+        /// <summary>
+        /// function argument from inputbinding, is "byte[] message"
+        /// </summary>
+        public byte[] ByteArrayMessage {get; init;}
+
+        /// <summary>
+        /// function argument from inputbinding, argiument is "string messageId"
+        /// </summary>
+        public string MessageId { get; init; }
+
+        /// <summary>
+        /// function argument from inputbinding, argiument is "IDictionary<string, object> UserProperties"
+        /// </summary>
+        public IDictionary<string, object> UserProperties { get; init; }
+
+        /// <summary>
+        /// Create new instance using information from function inputbinding
+        /// </summary>
+        /// <param name="byteArrayMessage">function argument from inputbinding, is "byte[] message"</param>
+        /// <param name="messageId">function argument from inputbinding, argiument is "string messageId"</param>
+        /// <param name="userProperties">function argument from inputbinding, argiument is "IDictionary<string, object> UserProperties"</param>
+        public FunctionInputData(byte[] byteArrayMessage, string messageId, IDictionary<string, object> userProperties)
+        {
+            ByteArrayMessage = byteArrayMessage;
+            MessageId = messageId;
+            UserProperties = new Dictionary<string, object>();
+            if (userProperties!=null)
+            {
+                foreach(var item in userProperties)
+                {
+                    UserProperties.Add(item.Key,item.Value);
+                }                 
+            }
+        }
+    }
+#endif
+}
---
+++ SBCompressor/Extensions/Reader/MessageReader.cs
@@ -0,0 +1,278 @@
+﻿#if NET6_0
+using Azure.Messaging.ServiceBus;
+#endif
+#if NETCOREAPP3_1 || NET5_0
+using Microsoft.Azure.ServiceBus;
+#endif
+using SBCompressor.Configuration;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SBCompressor.Extensions.Reader
+{
+    /// <summary>
+    /// Base class for message readers
+    /// </summary>
+    public abstract class MessageReader
+    {
+        /// <summary>
+        /// Default construcotr
+        /// </summary>
+        public MessageReader()
+        {
+            ChunkDictionary = new ConcurrentDictionary<string, List<byte[]>>();
+        }
+        /// <summary>
+        /// Constroctor with setting informations
+        /// </summary>
+        /// <param name="settingData"></param>
+        public MessageReader(StorageSettingData settingData)
+        {
+            ChunkDictionary = new ConcurrentDictionary<string, List<byte[]>>();
+            CurrentSettingData = settingData;
+        }
+
+        /// <summary>
+        /// Constroctor with setting informations
+        /// </summary>
+        /// <param name="settingData"></param>
+        /// <param name="typeToDeserialize">Type used to deserialize message</param>
+        public MessageReader(StorageSettingData settingData, Type typeToDeserialize)
+        {
+            ChunkDictionary = new ConcurrentDictionary<string, List<byte[]>>();
+            CurrentSettingData = settingData;
+            TypeToDeserialize = typeToDeserialize;
+        }
+
+
+        /// <summary>
+        /// Constroctor with setting informations
+        /// </summary>
+        /// <param name="typeToDeserialize">Type used to deserialize message</param>
+        public MessageReader(Type typeToDeserialize)
+        {
+            ChunkDictionary = new ConcurrentDictionary<string, List<byte[]>>();
+            TypeToDeserialize = typeToDeserialize;
+        }
+
+        private Type TypeToDeserialize { get; set; }
+
+        /// <summary>
+        /// Storage manager for very large message
+        /// </summary>
+        private MessageStorage storage;
+        /// <summary>
+        /// Storage manager for very large message
+        /// </summary>
+        internal protected MessageStorage Storage
+        {
+            get
+            {
+                if (storage == null)
+                {
+                    if (CurrentSettingData != null)
+                    {
+                        storage = new MessageStorage(CurrentSettingData);
+                    }
+                    else
+                    {
+                        storage = new MessageStorage();
+                    }
+                }
+                return storage;
+            }
+        }
+        /// <summary>
+        /// Container for chunks of large messages
+        /// </summary>
+        private ConcurrentDictionary<string, List<byte[]>> ChunkDictionary { get; set; }
+
+        /// <summary>
+        /// Setting informations
+        /// </summary>
+        private StorageSettingData CurrentSettingData { get; set; }
+
+#if NET5_0 || NETCOREAPP3_1
+        /// <summary>
+        /// Handler in caso of exceptions reading message
+        /// </summary>
+        /// <param name="exceptionReceivedEventArgs">information about the exceptions</param>
+        /// <returns></returns>
+        virtual protected Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            return Task.CompletedTask;
+        }
+#endif
+        /// <summary>
+        /// Handle the received message
+        /// </summary>
+        /// <param name="receivedMessage">message received from service bus</param>
+        /// <param name="token">CanellationToken</param>
+        /// <returns></returns>
+#if NET5_0 || NETCOREAPP3_1
+        virtual protected async Task MessageReceivedHandler(Message receivedMessage, CancellationToken token)
+        {
+            try
+            {
+                // Process message from subscription.
+                if (receivedMessage.UserProperties.ContainsKey(MessageFactory.MessageModePropertyName))
+                {
+                    var prop = receivedMessage.UserProperties[MessageFactory.MessageModePropertyName];
+#endif
+#if NET6_0
+        virtual protected async Task MessageReceivedHandler(ServiceBusReceivedMessage receivedMessage)//, CancellationToken token)
+        {
+            try
+            {
+                // Process message from subscription.
+                if (receivedMessage.ApplicationProperties.ContainsKey(MessageFactory.MessageModePropertyName))
+                {
+                    var prop = receivedMessage.ApplicationProperties[MessageFactory.MessageModePropertyName];
+#endif
+                    string propName = Convert.ToString(prop);
+                    if (!string.IsNullOrEmpty(propName))
+                    {
+                        MessageModes messageMode;
+                        bool parsed = Enum.TryParse(propName, out messageMode);
+                        if (parsed)
+                        {
+                            EventMessage msg = null;
+                            switch (messageMode)
+                            {
+                                case MessageModes.Simple:
+                                    msg = ReaderHandler.GetSimpleMessage(receivedMessage);
+                                    MessageReceived(msg, receivedMessage);
+                                    break;
+                                case MessageModes.GZip:
+                                    msg = await ReaderHandler.GetZippedMessage(receivedMessage);
+                                    MessageReceived(msg, receivedMessage);
+                                    break;
+                                case MessageModes.Chunk:
+                                    msg = await ReaderHandler.GetChunkedMessage(receivedMessage, ChunkDictionary);
+                                    if (msg != null)
+                                    {
+                                        MessageReceived(msg, receivedMessage);
+                                    }
+                                    break;
+                                case MessageModes.Storage:
+                                    msg = await ReaderHandler.GetStoredMessage(receivedMessage, Storage);
+                                    MessageReceived(msg, receivedMessage);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+#if NET6_0
+        virtual protected async Task MessageReceivedHandler(ProcessMessageEventArgs processMessage)//, CancellationToken token)
+        {
+            await MessageReceivedHandler(processMessage?.Message);
+        }
+#endif
+#if NET5_0 || NET6_0
+
+        /// <summary>
+        /// Handle the received message
+        /// </summary>
+        /// <param name="functionInputData">Data from the message by inpudbinding arguments</param>
+        /// <param name="token">CanellationToken</param>
+        /// <returns></returns>
+        virtual protected async Task MessageReceivedHandler(FunctionInputData functionInputData, CancellationToken token)
+        {
+            try
+            {
+                // Process message from subscription.
+                if (functionInputData.UserProperties.ContainsKey(MessageFactory.MessageModePropertyName))
+                {
+                    var prop = functionInputData.UserProperties[MessageFactory.MessageModePropertyName];
+                    string propName = Convert.ToString(prop);
+                    if (!string.IsNullOrEmpty(propName))
+                    {
+                        MessageModes messageMode;
+                        bool parsed = Enum.TryParse(propName, out messageMode);
+                        if (parsed)
+                        {
+                            EventMessage msg = null;
+                            switch (messageMode)
+                            {
+                                case MessageModes.Simple:
+                                    msg = ReaderHandler.GetSimpleMessage(functionInputData.ByteArrayMessage, TypeToDeserialize, MessageDeserializer);
+                                    MessageReceived(msg, functionInputData);
+                                    break;
+                                case MessageModes.GZip:
+                                    msg = await ReaderHandler.GetZippedMessage(functionInputData, TypeToDeserialize, MessageDeserializer);
+                                    MessageReceived(msg, functionInputData);
+                                    break;
+                                case MessageModes.Chunk:
+                                    msg = await ReaderHandler.GetChunkedMessage(functionInputData, ChunkDictionary, TypeToDeserialize, MessageDeserializer);
+                                    if (msg != null)
+                                    {
+                                        MessageReceived(msg, functionInputData);
+                                    }
+                                    break;
+                                case MessageModes.Storage:
+                                    msg = await ReaderHandler.GetStoredMessage(functionInputData, Storage, TypeToDeserialize, MessageDeserializer);
+                                    MessageReceived(msg, functionInputData);
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+#endif
+
+        /// <summary>
+        /// Action to invoke when a message arrive
+        /// </summary>
+        protected Action<MessageReceivedEventArgs> OnMessageReceived;
+
+
+        /// <summary>
+        /// Raise OnMessageReceived event
+        /// </summary>
+        /// <param name="message">Message managed by this library</param>
+        /// <param name="receivedMessage">Original message from queue or topic</param>
+#if NET5_0 || NETCOREAPP3_1
+        private void MessageReceived(EventMessage message, Message receivedMessage)
+#endif
+#if NET6_0
+        private void MessageReceived(EventMessage message, ServiceBusReceivedMessage receivedMessage)
+#endif
+        {
+            OnMessageReceived?.Invoke(new MessageReceivedEventArgs(message, receivedMessage));
+        }
+
+#if NET5_0 || NET6_0
+        /// <summary>
+        /// Raise OnMessageReceived event
+        /// </summary>
+        /// <param name="message">Message managed by this library</param>
+        /// <param name="functionInputData">Data from the message by inpudbinding arguments</param>
+        private void MessageReceived(EventMessage message, FunctionInputData functionInputData)
+        {
+            OnMessageReceived?.Invoke(new MessageReceivedEventArgs(message, functionInputData));
+        }
+#endif
+
+    }
+}
